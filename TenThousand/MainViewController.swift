d249fc01c9363916b3bd0e297bf20284d753003f
tavatar <ryo.tavatar@gmail.com>
2015-08-30 16:00:03 +0900

    new

---
+++ TenThousand/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TenThousand
+//
+//  Created by 田端 亮 on 2015/08/30.
+//  Copyright (c) 2015年 田端 亮. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
